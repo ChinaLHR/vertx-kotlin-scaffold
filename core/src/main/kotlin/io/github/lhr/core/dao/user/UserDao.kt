3f849c60d778cb4429c270d6816408f059e9d360
ChinaLHR <1420432344@qq.com>
2019-04-27 21:56:51 +0800

    fix

--- core/src/main/kotlin/io/github/lhr/core/dao/user/UserDao.kt
+++ core/src/main/kotlin/io/github/lhr/core/dao/user/UserDao.kt
@@ -1,7 +1,10 @@
 package io.github.lhr.core.dao.user
 
+import com.github.jasync.sql.db.util.flatMap
+import com.github.jasync.sql.db.util.map
 import io.github.lhr.core.dao.DbPool
 import io.github.lhr.core.domain.user.User
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
 
 
@@ -13,44 +16,41 @@ class UserDao {
 
     private var pool = DbPool.INSTANCE
 
-    fun findById(id: Long): User {
+    fun findById(id: Long): CompletableFuture<User> {
         val result = pool.connectionPool
                 .connect()
                 .get(5000, TimeUnit.MILLISECONDS)
                 .sendPreparedStatement("select id,name from user where id = ?", listOf(id))
-
-        val user = result.get()
-                .rows
+                .map { it.rows.first() }
                 .map { User(it.getLong("id")!!, it.getString("name")!!) }
-                .first()
 
-        return user
+
+        return result
     }
 
-    fun insertUser(name: String) {
-        pool.connectionPool
+    fun insertUser(name: String): CompletableFuture<Long> {
+        return pool.connectionPool
                 .connect()
                 .get(5000, TimeUnit.MILLISECONDS)
                 .sendPreparedStatement("insert into user (name) values (?)", listOf(name))
+                .map { it.rowsAffected }
     }
 
-    fun updateUser(user: User) {
-        pool.connectionPool
+    fun updateUser(user: User): CompletableFuture<Long> {
+        return pool.connectionPool
                 .connect()
                 .get(5000, TimeUnit.MILLISECONDS)
                 .sendPreparedStatement("update user set name = ? where id = ?", listOf(user.name, user.id))
+                .map { it.rowsAffected }
     }
 
-    fun findAll(): List<User> {
-        val result = pool.connectionPool
+    fun findAll(): CompletableFuture<User> {
+        return pool.connectionPool
                 .connect()
                 .get(5000, TimeUnit.MILLISECONDS)
                 .sendQuery("select id,name from user")
+                .map { it.rows[0] }
+                .map { User(it.getLong("id")!!, it.getString("name")!!) }
 
-        val userList = result.get().rows.map {
-            User(it[0] as Long, it[1] as String)
-        }
-
-        return userList
     }
 }
