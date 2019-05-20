fccb3f63ad7582775884cbdcce1dbf9af35b6602
ChinaLHR <1420432344@qq.com>
2019-05-20 23:36:16 +0800

    fix

--- api/src/main/kotlin/io/github/lhr/api/routes/Routes.kt
+++ api/src/main/kotlin/io/github/lhr/api/routes/Routes.kt
@@ -1,5 +1,6 @@
 package io.github.lhr.api.routes
 
+import io.github.lhr.api.handler.UserHandler
 import io.github.lhr.core.common.CoroutineRoute
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
@@ -10,9 +11,16 @@ import io.vertx.ext.web.Router
  * @date 2019/5/20
  */
 
-class Routes(val vertx: Vertx): CoroutineRoute(vertx) {
-    fun createRouter(): Router {
-        val subRouter = Router.router(vertx)
+class Routes(val vertx: Vertx) : CoroutineRoute(vertx) {
 
+    fun initRoute(router: Router){
+        val userHandler = UserHandler()
+
+        val subRouter = Router.router(vertx)
+        val apply = Router.router(vertx).apply {
+            subRouter.route("/User/findAll").coroutineHandler { ctx -> userHandler.findAll(ctx) }
+        }
+
+        router.mountSubRouter("/api", apply)
     }
 }
