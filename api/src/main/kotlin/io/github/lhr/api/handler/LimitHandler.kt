98e963fb8c34e34eced0e0de9967fd01b6205ee9
ChinaLHR <1420432344@qq.com>
2019-05-29 23:12:31 +0800

    fix

--- api/src/main/kotlin/io/github/lhr/api/handler/LimitHandler.kt
+++ api/src/main/kotlin/io/github/lhr/api/handler/LimitHandler.kt
@@ -1,6 +1,7 @@
 package io.github.lhr.api.handler
 
 import com.google.common.util.concurrent.RateLimiter
+import io.github.lhr.core.ext.ok
 import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
 import org.slf4j.LoggerFactory
@@ -11,18 +12,36 @@ import org.slf4j.LoggerFactory
  * @date 2019/5/28
  *
  * 限流Handler
+ * todo lhr 待优化 只针对部分handler做限流
  */
 class LimitHandler : Handler<RoutingContext> {
 
     val logger = LoggerFactory.getLogger(LoggerFactory::class.java)
 
-    var ipLimitMap = mapOf<String, RateLimiter>()
+    /**
+     * key = uri-host value rateLimiter
+     */
+    var ipLimitMap = mutableMapOf<String, RateLimiter>()
 
-    override fun handle(event: RoutingContext) {
-        val host = event.request().remoteAddress().host()
-        val uri = event.request().uri()
-        logger.info("key:$host:$uri")
-        event.next()
+    override fun handle(ctx: RoutingContext) {
+        val host = ctx.request().remoteAddress().host()
+        val rateLimiter = ipLimitMap[host]
+
+        if (rateLimiter==null){
+            /**
+             * ip qps限制为10
+             */
+            val limiter = RateLimiter.create(10.0)
+            ipLimitMap[host] = limiter
+
+        }else{
+            val tryAcquire = rateLimiter.tryAcquire()
+            if (!tryAcquire) {
+                ctx.ok("请求限制,请稍后重试")
+                return
+            }
+        }
+        ctx.next()
     }
 
 
