12a048fdf186fe157ae55be0ef4f4ecac2cafd29
ChinaLHR <1420432344@qq.com>
2019-08-07 17:40:44 +0800

    opt sql

--- doc/ddl.sql
+++ doc/ddl.sql
@@ -3,5 +3,8 @@ use test;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT '',
+  `sex_type` tinyint(4) NOT NULL DEFAULT '-1' COMMENT '性别. -1,未知; 0,女; 1,男',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
