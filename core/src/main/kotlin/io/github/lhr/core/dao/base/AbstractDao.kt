97465786d048de01a34a2a4a89b1ddadfed49ce9
ChinaLHR <1420432344@qq.com>
2019-08-29 18:32:18 +0800

    opt AbstractDao

--- core/src/main/kotlin/io/github/lhr/core/dao/base/AbstractDao.kt
+++ core/src/main/kotlin/io/github/lhr/core/dao/base/AbstractDao.kt
@@ -2,28 +2,26 @@ package io.github.lhr.core.dao.base
 
 import io.github.lhr.core.domain.entity.converters.ModelConverter
 import io.vertx.core.json.JsonObject
-import io.vertx.kotlin.ext.mongo.findAwait
-import io.vertx.kotlin.ext.mongo.findOneAwait
-import io.vertx.kotlin.ext.mongo.removeDocumentAwait
-import io.vertx.kotlin.ext.mongo.saveAwait
+import io.vertx.ext.mongo.BulkOperation
+import io.vertx.kotlin.ext.mongo.*
 
 /**
  * @Author : lhr
  * @Date : 18:35 2019/8/26
  */
-abstract class AbstractDao<T, ID>(val collection: String)  {
+abstract class AbstractDao<T, ID>(val collection: String) {
 
-    suspend inline fun <reified T>list(query: JsonObject = JsonObject()): List<T> {
+    suspend inline fun <reified T> list(query: JsonObject = JsonObject()): List<T> {
         val result = mongoClient.findAwait(collection, query)
         return ModelConverter.fromJson(result)
     }
 
-    suspend inline fun <reified T>save(entity: T): String? {
+    suspend inline fun <reified T> save(entity: T): String? {
         val document = ModelConverter.toJson(entity)
         return mongoClient.saveAwait(collection, document)
     }
 
-    suspend inline fun <reified T>read(id: ID, fields: JsonObject = JsonObject()): T {
+    suspend inline fun <reified T> read(id: ID, fields: JsonObject = JsonObject()): T {
         val query = JsonObject().put("_id", id)
         val result = mongoClient.findOneAwait(collection, query, fields) ?: JsonObject()
         return ModelConverter.fromJson(result)
@@ -32,5 +30,12 @@ abstract class AbstractDao<T, ID>(val collection: String)  {
     suspend fun delete(id: ID) {
         val query = JsonObject().put("_id", id)
         mongoClient.removeDocumentAwait(collection, query)
+    }
+
+    /**
+     * 批量insert update replace  delete
+     */
+    suspend fun bulkWrite(operations: List<BulkOperation>) {
+        mongoClient.bulkWriteAwait(collection, operations)
     }
 }
